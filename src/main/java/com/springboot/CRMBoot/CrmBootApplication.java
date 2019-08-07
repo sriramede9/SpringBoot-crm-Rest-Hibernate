848f9e6a1283694c67b69c8ab95f495fc84e9554
sriramede9 <only4programming03@gmail.com>
2019-08-06 22:03:00 -0400

    inital setup #jpa #web #security #mysql-driver #dev-tools from spring initializer

---
+++ src/main/java/com/springboot/CRMBoot/CrmBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.CRMBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBootApplication.class, args);
+	}
+
+}
